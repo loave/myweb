48f7a5405e26c1066c15529a9f8b9b4d36aad0b6
loave <jamesraul.7@163.com>
2018-10-19 23:55:17 +0800

    添加unittest部分

--- blog/tests.py
+++ blog/tests.py
@@ -10,8 +10,11 @@ class AuthorTestCase(TestCase):
         Author.objects.create(first_name='zhang',last_name='san',email='zhangsan@gmail.com')
         Author.objects.create(first_name='li',last_name='si',email='lisi@gmail.com')
 
-    def test_add_author_email(self):
+    def test_add_author_zhangsan_email(self):
         zhang=Author.objects.get(first_name='zhang')
-        li=Author.objects.get(first_name='li')
-        self.assertEqual(zhang.email,'zhangsan1@gmail.com')
-        self.assertEqual(li.email,'lisi@gmail.com')
+        self.assertEqual(zhang.email,'zhangsan@gmail.com')
+
+
+    def test_add_author_lisi_email(self):
+        li = Author.objects.get(first_name='li')
+        self.assertEqual(li.email, 'lisi@gmail.com')
